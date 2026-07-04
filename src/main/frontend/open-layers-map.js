063b678da4b18ae402abd349bc7add0d7d071c10
Thomas Siegenthaler <thomas.siegenthaler@email.com>
2026-07-04 15:52:53 +0200

    Icon hover

--- src/main/frontend/open-layers-map.js
+++ src/main/frontend/open-layers-map.js
@@ -133,6 +133,18 @@ class OpenLayersMap extends LitElement {
         margin-bottom: 4px;
         border: 2px solid transparent;
         background-size: cover;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .layer-icon-preview svg {
+        width: 24px;
+        height: 24px;
+        fill: none;
+        stroke: currentColor;
+        stroke-width: 2;
+        stroke-linecap: round;
+        stroke-linejoin: round;
       }
       .layer-option.active .layer-icon-preview {
         border-color: #2563eb;
@@ -183,15 +195,35 @@ class OpenLayersMap extends LitElement {
         </button>
         <div class="layer-menu ${this._menuOpen ? 'open' : ''}">
           <div class="layer-option ${this.baseStyle === 'streets' ? 'active' : ''}" @click=${(e) => this._handleUiClick(e, () => this._selectStyle('streets'))}>
-            <div class="layer-icon-preview" style="background-image: url('https://abc-atp.github.io/images/map-streets.png'), linear-gradient(#e0e0e0, #bdbdbd);"></div>
+            <div class="layer-icon-preview" style="background: linear-gradient(#e0e0e0, #bdbdbd); color: #374151;">
+              <svg viewBox="0 0 24 24">
+                <path d="M14.106 5.553a2 2 0 0 0 1.788 0l3.659-1.83A1 1 0 0 1 21 4.619v12.764a1 1 0 0 1-.553.894l-4.553 2.277a2 2 0 0 1-1.788 0l-4.212-2.106a2 2 0 0 0-1.788 0l-3.659 1.83A1 1 0 0 1 3 19.381V6.618a1 1 0 0 1 .553-.894l4.553-2.277a2 2 0 0 1 1.788 0z"/>
+                <path d="M15 5.764v15.236"/>
+                <path d="M9 3.236v15.236"/>
+              </svg>
+            </div>
             <span>Straße</span>
           </div>
           <div class="layer-option ${this.baseStyle === 'satellite' ? 'active' : ''}" @click=${(e) => this._handleUiClick(e, () => this._selectStyle('satellite'))}>
-            <div class="layer-icon-preview" style="background-image: url('https://abc-atp.github.io/images/map-satellite.png'), linear-gradient(#424242, #212121);"></div>
+            <div class="layer-icon-preview" style="background: linear-gradient(#424242, #212121); color: #f4f4f5;">
+              <svg viewBox="0 0 24 24">
+                <path d="M13 7 9 3 4 8l4 4"/>
+                <path d="m17 11 4 4-5 5-4-4"/>
+                <path d="m8 12 4 4"/>
+                <path d="m16 8 3-3"/>
+                <path d="M9 21a6 6 0 0 0-6-6"/>
+              </svg>
+            </div>
             <span>Satellit</span>
           </div>
           <div class="layer-option ${this.baseStyle === 'hybrid' ? 'active' : ''}" @click=${(e) => this._handleUiClick(e, () => this._selectStyle('hybrid'))}>
-            <div class="layer-icon-preview" style="background-image: url('https://abc-atp.github.io/images/map-hybrid.png'), linear-gradient(#757575, #424242);"></div>
+            <div class="layer-icon-preview" style="background: linear-gradient(#757575, #424242); color: #f4f4f5;">
+              <svg viewBox="0 0 24 24">
+                <path d="m12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83Z"/>
+                <path d="m22 17.65-9.17 4.16a2 2 0 0 1-1.66 0L2 17.65"/>
+                <path d="m22 12.65-9.17 4.16a2 2 0 0 1-1.66 0L2 12.65"/>
+              </svg>
+            </div>
             <span>Hybrid</span>
           </div>
         </div>
